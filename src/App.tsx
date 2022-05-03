7e917c3926a05da3f42db1f2d747679eedc1438c
wesleyara <wes1gom@gmail.com>
2022-05-02 23:31:42 -0300

    feat: Adicionando tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 
+import "./styles/globals.css";
+
 function App() {
   return (
     <Router>
